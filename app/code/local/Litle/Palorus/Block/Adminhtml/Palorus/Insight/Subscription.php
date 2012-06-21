282c3965bd40004a9cd82efd2c2089948e46f026
Kush, Patel <kpatel@litle.com>
2012-06-21 13:15:16 -0400

    updated subscription.php

--- app/code/local/Litle/Palorus/Block/Adminhtml/Palorus/Insight/Subscription.php
+++ app/code/local/Litle/Palorus/Block/Adminhtml/Palorus/Insight/Subscription.php
@@ -41,127 +41,98 @@ implements Mage_Adminhtml_Block_Widget_Tab_Interface {
 	 * Set the template for the block
 	 *
 	 */
-	protected function _construct()
+	public function _construct()
 	{
 		parent::_construct();
-		$this->setTemplate('Subscriptions/form/Subscriptions.phtml');
+		$this->setId('litle_customer_orders_grid');
+		$this->setDefaultSort('order_number', 'desc');
+		$this->setUseAjax(true);
+		$this->setPagerVisibility(false);
+		$this->setFilterVisibility(false);
 	}
-	protected function _toHtml()
+
+	protected function _prepareCollection()
 	{
-		Mage::dispatchEvent('payment_form_block_to_html_before', array(
-	            'block'     => $this
-		));
-		return parent::_toHtml();
-	}
-// 	public function _construct()
-// 	{
-// 		parent::_construct();
-// 		$this->setId('litle_customer_orders_grid');
-// 		$this->setDefaultSort('order_number', 'desc');
-// 		$this->setUseAjax(true);
-// 		$this->setPagerVisibility(false);
-// 		$this->setFilterVisibility(false);
-// 	}
-
-// 	protected function _prepareCollection()
-// 	{
-// 		$customerId = Mage::registry('current_customer')->getId();
-// 		$collection = Mage::getModel('palorus/insight')
-// 			->getCollection()
-// 			->addFieldToFilter('customer_id',$customerId);
+		$customerId = Mage::registry('current_customer')->getId();
+		$subscriptionId = Mage::registry('current_customer')->getId();
+		$collection = Mage::getModel('palorus/subscription')
+			->getCollection()
+			->addFieldToFilter('customer_id',$customerId);
 			
-// 		$this->setCollection($collection);
-// 		return parent::_prepareCollection();
-// 	}
+		$this->setCollection($collection);
+		return parent::_prepareCollection();
+	}
 
-// 	protected function _prepareColumns()
-// 	{
-// 		$this->addColumn('order_number', array(
-//                 'header'    => 'Order Number',
-//                 'width'     => '100',
-//                 'index'     => 'order_number',
-//                 'sortable'		=> false,
-// 		));
-// 		$this->addColumn('last', array(
-//     	        'header'    => 'Last 4',
-// 	            'width'     => '100',
-//                 'index'     => 'last',
-//                 'sortable'		=> false,
-// 		));
-// 		$this->addColumn('order_amount', array(
-//                'header'    => 'Order Amount',
+	protected function _prepareColumns()
+	{
+		$this->addColumn('subscription_Id', array(
+                'header'    => 'Subscription ID',
+                'width'     => '100',
+                'index'     => 'subscription_Id',
+                'sortable'		=> false,
+		));
+		$this->addColumn('product_id', array(
+    	        'header'    => 'Product Id',
+	            'width'     => '100',
+                'index'     => 'product_id',
+                'sortable'		=> false,
+		));
+		$this->addColumn('start_date', array(
+				'header'    => 'Product Name',
+				'width'     => '100',
+				'index'     => 'order_amount',
+				'sortable'		=> false,
+		));
+// 		$this->addColumn('start_date', array(
+//                'header'    => 'Start Date',
 //                'width'     => '100',
-//                'index'     => 'order_amount',
+//                'index'     => 'start_date',
 //                'sortable'		=> false,
 // 		));
-// 		$this->addColumn('affluence', array(
-//                'header'    => 'Affluence',
-//                'width'     => '100',
-//                'index'     => 'affluence',
-//                'sortable'		=> false,
-// 		));
-// 		$this->addColumn('issuing_country', array(
-//                'header'    => 'Issuing Country',
-//                'width'     => '100',
-//                'index'     => 'issuing_country',
-//                'sortable'		=> false,
-// 		));
-// 		$this->addColumn('prepaid_card_type', array(
-//                'header'    => 'Prepaid Card Type',
-//                'width'     => '100',
-//                'index'     => 'prepaid_card_type',
-//                'sortable'		=> false,
-// 		));
-// 		$this->addColumn('funding_source', array(
-//                'header'    => 'Funding Source',
-//                'width'     => '100',
-//                'index'     => 'funding_source',
-//                'sortable'		=> false,
-// 		));
-// 		$this->addColumn('available_balance', array(
-//                'header'    => 'Available Balance',
-//                'width'     => '100',
-//                'index'     => 'available_balance',
-//                'sortable'		=> false,
-// 		));
-// 		$this->addColumn('reloadable', array(
-//                'header'    => 'Reloadable',
-//                'width'     => '100',
-//                'index'     => 'reloadable',
-//                'sortable'		=> false,
-// 		));
-// 		return parent::_prepareColumns();
-// 	}
+		$this->addColumn('iteration_length', array(
+               'header'    => 'Billing Cycle Period',
+               'width'     => '100',
+               'index'     => 'iteration_length',
+               'sortable'		=> false,
+		));
+		$this->addColumn('next_bill_date', array(
+               'header'    => 'Next Bill Date',
+               'width'     => '100',
+               'index'     => 'next_bill_date',
+               'sortable'		=> false,
+		));
+		return parent::_prepareColumns();
+	}
 	
-// 	public function getRowUrl($row)
-// 	{
-// 		return $this->getUrl('*/sales_order/view', array('order_id' => $row->getOrderId()));
-// 	}
+	public function getRowUrl($row)
+	{
+		return $this->getUrl('*/sales_order/view', array('order_id' => $row->getOrderId()));
+	}
 	
-// 	public function getGridUrl()
-// 	{
-// // 		Mage::log("Get grid url");
-// // 		return $this->getUrl('*/*/orders', array('_current' => true));
-// 	}
+	public function getGridUrl()
+	{
+// 		Mage::log("Get grid url");
+// 		return $this->getUrl('*/*/orders', array('_current' => true));
+	}
 
 	/**
 	 * Retrieve the label used for the tab relating to this block
 	 *
 	 * @return string
- 	 */
+	 */
 	public function getTabLabel()
 	{
-		return $this->__('Litle & Co. Subscription Insight');
+		return $this->__('Litle & Co. Subscription');
 	}
 
-// 	/**
-// 	 * Retrieve the title used by this tab
-// 	 *
-// 	 * @return string
-// 	 */
+	/**
+	 * Retrieve the title used by this tab
+	 *
+	 * @return string
+	 */
 	public function getTabTitle()
 	{
-		return $this->__('Click here to view Litle & Co. Subscription Insight');
+		return $this->__('Click here to view Litle & Co. Subscription');
 	}
 
 	/**
@@ -184,5 +155,6 @@ implements Mage_Adminhtml_Block_Widget_Tab_Interface {
 	{
 		return false;
 	}
+
 
 }
