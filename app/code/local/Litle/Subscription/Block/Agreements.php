3145688fc3d922d5e4e06e48e9ea335da33e5a1c
arch85 <archit.agarwal@gmail.com>
2012-07-02 05:48:53 -0400

    Agreements show up only when Litle Subscription MOP is active, AND "disable subscription agreement" is set to "no".

--- app/code/local/Litle/Subscription/Block/Agreements.php
+++ app/code/local/Litle/Subscription/Block/Agreements.php
@@ -4,26 +4,34 @@ class Litle_Subscription_Block_Agreements extends Mage_Checkout_Block_Agreements
 {
     public function getAgreements()
     {
-    	$litleAgreement = new Varien_Object();
-    	$litleAgreement->setAgreementId(39);
-    	$litleAgreement->setName("Lit2");
-    	$litleAgreement->setContent("bleep blop tip top");
-    	$litleAgreement->setContentHeight(NULL);
-    	$litleAgreement->setCheckboxText("I agree");
-    	$litleAgreement->setIsActive(1);
-    	$litleAgreement->setIsHtml(0);
-    	
-		if (!$this->hasAgreements()) {
-        	if (!Mage::getStoreConfigFlag('checkout/options/enable_agreements')) {
-        		$agreements = new Varien_Data_Collection();
-            } else {
-            	$agreements = Mage::getModel('checkout/agreement')->getCollection()
-                    ->addStoreFilter(Mage::app()->getStore()->getId())
-                    ->addFieldToFilter('is_active', 1);
-            }
-           	$agreements->addItem($litleAgreement);
-            $this->setAgreements($agreements);
-        }
-        return $this->getData('agreements');
+    	if(Mage::getStoreConfig('payment/Subscription/active') == 0 || 
+    	   (Mage::getStoreConfig('payment/Subscription/active') == 1 && Mage::getStoreConfig('payment/Subscription/disablesubsagreement') == 1)
+    	  )
+    	{
+    		return parent::getAgreements();
+    	}
+    	else{
+    		$litleAgreement = new Varien_Object();
+	    	$litleAgreement->setAgreementId(39);
+	    	$litleAgreement->setName("Subscription Agreement");
+	    	$litleAgreement->setContent("bleep blop tip top");
+	    	$litleAgreement->setContentHeight(NULL);
+	    	$litleAgreement->setCheckboxText("I agree");
+	    	$litleAgreement->setIsActive(1);
+	    	$litleAgreement->setIsHtml(0);
+	    	
+			if (!$this->hasAgreements()) {
+	        	if (!Mage::getStoreConfigFlag('checkout/options/enable_agreements')) {
+	        		$agreements = new Varien_Data_Collection();
+	            } else {
+	            	$agreements = Mage::getModel('checkout/agreement')->getCollection()
+	                    ->addStoreFilter(Mage::app()->getStore()->getId())
+	                    ->addFieldToFilter('is_active', 1);
+	            }
+	           	$agreements->addItem($litleAgreement);
+	            $this->setAgreements($agreements);
+	        }
+	        return $this->getData('agreements');
+    	}
     }
 }
